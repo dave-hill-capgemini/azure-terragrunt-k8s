70f1d60413b4a43b7114710c54ecaa3b650946d0
Dave Hill <dave.hill@capgemini.com>
2023-05-12 12:06:35 +0100

    az get-creds cli command as output

--- modules/azure-terraform-aks/outputs.tf
+++ modules/azure-terraform-aks/outputs.tf
@@ -5,3 +5,7 @@ output "resource_group_name" {
 output "kubernetes_cluster_name" {
   value = azurerm_kubernetes_cluster.this.name
 }
+
+output "kubeconfig" {
+  value = "az aks get-credentials --admin --name ${replace(var.location, "/\\s+/", "")}-${var.env}-k8s --resource-group  ${replace(var.location, "/\\s+/", "")}-${var.env}-k8s-rg"
+}
